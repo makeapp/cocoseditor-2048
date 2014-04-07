41d13ff2ba285d104f7a53a2e9ac3db807ea3965
touchsnow <zuowen@makeapp.co>
2014-04-07 14:47:05 +0800

    touchsnow

---
+++ 2048/Published/CocosEditor.js
@@ -0,0 +1,292 @@
+/**
+ * @GameName :
+ * common function
+ *
+ * @DevelopTool:
+ * Cocos2d-x Editor (CocosEditor)
+ *
+ * @time
+ * 2014-04-04 pm
+ *
+ * @Licensed:
+ * This showcase is licensed under GPL.
+ *
+ * @Authors:
+ * Programmer: touchSnow
+ *
+ * @Links:
+ * http://www.cocos2d-x.com/ (cocos官方)
+ * https://github.com/makeapp      （github）
+ * http://blog.csdn.net/touchsnow (csdn博客)
+ * http://blog.makeapp.co/ （官方博客）
+ * http://www.cocoseditor.com/ （建设中官网）
+ *
+ * @Contact
+ * 邮箱：zuowen@makeapp.co
+ * qq群：232361142
+ *
+ */
+
+cc.debug = function (msg) {
+    cc.log(msg);
+};
+
+cc.Animate.createWithName = function (name) {
+    return cc.Animate.create(cc.AnimationCache.getInstance().getAnimation(name));
+};
+
+cc.Animate.createWithTime = function (that, name, time) {
+    var cache = that.animationCache.getAnimation(name);
+    cache.setDelayPerUnit(time);
+    return cc.Animate.create(cache);
+};
+
+cc.CleanUp = {};
+cc.CleanUp.create = function (sprite) {
+    return cc.CallFunc.create(function () {
+        sprite.cleanuped = true;
+        sprite.removeFromParent(true);
+    });
+};
+
+
+cc.TurnLeft = {};
+cc.TurnLeft.create = function (sprite) {
+    return cc.CallFunc.create(function () {
+        var camera = sprite.getCamera();
+        camera.setEye(0, 0, 1);
+    });
+};
+
+cc.TurnRight = {};
+cc.TurnRight.create = function (sprite) {
+    return cc.CallFunc.create(function () {
+        var camera = sprite.getCamera();
+        camera.setEye(0, 0, -1);
+    });
+};
+
+cc.Toast = {};
+cc.Toast.create = function (node, message, delay) {
+    var director = cc.Director.getInstance();
+    var size = director.getWinSize();
+    var label = cc.LabelTTF.create(message, "Arial", 40);
+    label.setPosition(size.width / 2, size.height / 2 + 410);
+    label.setColor(cc.c3b(0, 0, 0));
+    label.setZOrder(10000);
+    node.addChild(label);
+    label.runAction(cc.Sequence.create(cc.DelayTime.create(delay), cc.CleanUp.create(label)));
+    return label;
+};
+
+
+cc.MySprite = {};
+cc.MySprite.create = function (node, frameName, position, ZOrder) {
+    var sprite = cc.Sprite.createWithSpriteFrameName(frameName);
+    sprite.setPosition(position);
+    sprite.setZOrder(ZOrder)
+    sprite.setAnchorPoint(cc.p(0.5, 0.5));
+    node.addChild(sprite);
+    return sprite;
+};
+
+cc.MySprite.createLabel = function (node, number) {
+    var label = cc.LabelTTF.create(number, "Arial", 50);
+    label.setPosition(81, 81);
+    label.setColor(cc.c3b(0, 0, 0));
+    node.addChild(label);
+    return label;
+};
+
+cc.rectCreate = function (p, area) {
+    return  cc.rect(p.x - area[0], p.y - area[1], area[0] * 2, area[1] * 2);
+}
+
+//get index score
+sys.localStorage.getScore = function (name, index) {
+    var score = sys.localStorage.getItem(name);
+    if (score != null && score != undefined && score != "") {
+        score = Number(score);
+    }
+    else {
+        score = index;
+    }
+    return score;
+};
+
+//set level score and open next level
+sys.localStorage.setLevelScore = function (name, curLevel, curScore) {
+    var localCurLevel = sys.localStorage.getItem(name + "_current_level");
+    cc.log("localCurLevel==" + localCurLevel);
+    localCurLevel = Number(localCurLevel);
+    if (curLevel >= localCurLevel) {
+        var newCurLevel = curLevel + 1;
+        sys.localStorage.setItem(name + "_current_level", newCurLevel);
+    }
+
+    var currentLevelScore = sys.localStorage.getItem(name + "_level_" + curLevel);
+    if (curScore > currentLevelScore) {
+        sys.localStorage.setItem(name + "_level_" + curLevel, curScore);
+    }
+};
+
+
+cc.BuilderReader.replaceScene = function (path, ccbName) {
+    var scene = cc.BuilderReader.loadAsSceneFrom(path, ccbName);
+    cc.Director.getInstance().replaceScene(scene);
+    return scene;
+};
+
+cc.BuilderReader.loadAsScene = function (file, owner, parentSize) {
+    var node = cc.BuilderReader.load(file, owner, parentSize);
+    var scene = cc.Scene.create();
+    scene.addChild(node);
+    return scene;
+};
+
+
+cc.BuilderReader.loadAsSceneFrom = function (path, ccbName) {
+    if (path && path.length > 0) {
+        cc.BuilderReader.setResourcePath(path + "/");
+        return cc.BuilderReader.loadAsScene(path + "/" + ccbName);
+    }
+    else {
+        return cc.BuilderReader.loadAsScene(ccbName);
+    }
+};
+
+cc.BuilderReader.loadAsNodeFrom = function (path, ccbName, owner) {
+    if (path && path.length > 0) {
+        cc.BuilderReader.setResourcePath(path + "/");
+        return cc.BuilderReader.load(path + "/" + ccbName, owner);
+    }
+    else {
+        return cc.BuilderReader.load(ccbName, owner);
+    }
+};
+
+cc.BuilderReader.runScene = function (module, name) {
+    var director = cc.Director.getInstance();
+    var scene = cc.BuilderReader.loadAsSceneFrom(module, name);
+    var runningScene = director.getRunningScene();
+    if (runningScene === null) {
+        cc.log("runWithScene");
+        director.runWithScene(scene);
+    }
+    else {
+        cc.log("replaceScene");
+        director.replaceScene(scene);
+    }
+};
+
+cc.getRandomFromMax = function (maxSize) {
+    return Math.floor(Math.random() * maxSize) % maxSize;
+};
+
+//get index score
+sys.localStorage.getScore = function (name, index) {
+    var score = sys.localStorage.getItem(name);
+    if (score != null && score != undefined && score != "") {
+        score = Number(score);
+    }
+    else {
+        score = index;
+    }
+    return score;
+};
+
+//set level score and open next level
+sys.localStorage.setLevelScore = function (name, curLevel, curScore) {
+    var localCurLevel = sys.localStorage.getItem(name + "_current_level");
+    cc.log("localCurLevel==" + localCurLevel);
+    localCurLevel = Number(localCurLevel);
+    if (curLevel >= localCurLevel) {
+        var newCurLevel = curLevel + 1;
+        sys.localStorage.setItem(name + "_current_level", newCurLevel);
+    }
+
+    var currentLevelScore = sys.localStorage.getItem(name + "_level_" + curLevel);
+    if (curScore > currentLevelScore) {
+        sys.localStorage.setItem(name + "_level_" + curLevel, curScore);
+    }
+};
+
+function getRandom(maxSize) {
+    return Math.floor(Math.random() * maxSize) % maxSize;
+}
+
+String.prototype.isValid = function () {
+    if (this == undefined || this == null) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+Array.prototype.contains = function (value) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] == value) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Array.prototype.position = function (value) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+getTimeFormat = function (time) {
+    if (time <= 9) {
+        return "0" + time;
+    }
+    else {
+        return "" + time;
+    }
+};
+
+function getHourRemainTime(time) {
+    var t = time.split(/[- :]/);
+    var expireDate = new Date(t[0], t[1] - 1, t[2], t[3], t[4], t[5]);
+    var nowDate = new Date();
+    var seconds = (expireDate.getTime() - nowDate.getTime()) / 1000;
+    var iHour = Math.floor(seconds / 3600);
+    var iMin = Math.floor((seconds - iHour * 3600) / 60);
+    var iSen = Math.floor(seconds - iHour * 3600 - iMin * 60);
+    var remainTime = getTimeFormat(iHour) + ":" + getTimeFormat(iMin) + ":" + getTimeFormat(iSen);
+    return  remainTime;
+}
+
+function getMinuteRemainTime(time) {
+    var t = time.split(/[- :]/);
+    var expireDate = new Date(t[0], t[1] - 1, t[2], t[3], t[4], t[5]);
+    var nowDate = new Date();
+    var seconds = (expireDate.getTime() - nowDate.getTime()) / 1000;
+    var iHour = Math.floor(seconds / 3600);
+    var iMin = Math.floor((seconds - iHour * 3600) / 60);
+    var iSen = Math.floor(seconds - iHour * 3600 - iMin * 60);
+    var remainTime = getTimeFormat(iMin) + ":" + getTimeFormat(iSen);
+    return  remainTime;
+}
+
+cc.Button = {};
+cc.Button.standardButtonWithTitle = function (normal, select, title) {
+    /** Creates and return a button with a default background and title color. */
+    var backgroundButton = cc.Scale9Sprite.create(normal);
+    backgroundButton.setPreferredSize(cc.size(200, 200));
+    var backgroundHighlightedButton = cc.Scale9Sprite.create(select);
+    backgroundHighlightedButton.setPreferredSize(cc.size(200, 200));
+    var titleButton = cc.LabelTTF.create(title, "Marker Felt", 30);
+    titleButton.setColor(cc.c3b(159, 168, 176));
+
+    var button = cc.ControlButton.create(titleButton, backgroundButton);
+    button.setBackgroundSpriteForState(backgroundHighlightedButton, cc.CONTROL_STATE_NORMAL);
+    button.setTitleColorForState(cc.WHITE, cc.CONTROL_STATE_NORMAL);
+    return button;
+}
---
+++ 2048/Published/MainLayer.js
@@ -0,0 +1,411 @@
+/**
+ * @GameName :
+ * 2048
+ *
+ * @DevelopTool:
+ * Cocos2d-x Editor (CocosEditor)
+ *
+ * @time
+ * 2014-04-04 pm
+ *
+ * @Licensed:
+ * This showcase is licensed under GPL.
+ *
+ * @Authors:
+ * Programmer: touchSnow
+ *
+ * @Links:
+ * http://www.cocos2d-x.com/ (cocos官方)
+ * https://github.com/makeapp      （github）
+ * http://blog.csdn.net/touchsnow (csdn博客)
+ * http://blog.makeapp.co/ （官方博客）
+ * http://www.cocoseditor.com/ （建设中官网）
+ *
+ * @Contact
+ * 邮箱：zuowen@makeapp.co
+ * qq群：232361142
+ *
+ */
+
+
+var MainLayer = function () {
+    this.initX = 9;
+    this.initY = 300;
+    this.cellSize = 162;
+    this.cellSpace = 18;
+    this.totalScore = 0;
+    this.scoreLabel = {};
+
+    this.back = {};
+    this.title = {};
+};
+
+MainLayer.prototype.onDidLoadFromCCB = function () {
+    if (sys.platform == 'browser') {
+        this.onEnter();
+    }
+    else {
+        this.rootNode.onEnter = function () {
+            this.controller.onEnter();
+        };
+    }
+
+    this.rootNode.onTouchesBegan = function (touches, event) {
+        this.controller.onTouchesBegan(touches, event);
+        return true;
+    };
+
+    this.rootNode.onTouchesMoved = function (touches, event) {
+        this.controller.onTouchesMoved(touches, event);
+        return true;
+    };
+    this.rootNode.onTouchesEnded = function (touches, event) {
+        this.controller.onTouchesEnded(touches, event);
+        return true;
+    };
+
+    this.rootNode.setTouchEnabled(true);
+};
+
+MainLayer.prototype.getPosition = function (i, j) {
+    var px = this.initX + this.cellSize / 2 + i * (this.cellSize + this.cellSpace);
+    var py = this.initY + this.cellSize / 2 + j * (this.cellSize + this.cellSpace);
+    return cc.p(px, py);
+}
+
+MainLayer.prototype.onEnter = function () {
+    //version
+    this.versionNum = indexVersions;
+    this.indexVersion = VERSIONS[this.versionNum];
+    this.title.setString(this.indexVersion.name + "目标：" + this.indexVersion.array[this.indexVersion.array.length - 1] + "");
+
+    var random1 = getRandom(4);
+    var random2 = getRandom(4);
+    while (random1 == random2) {
+        random2 = getRandom(4);
+    }
+
+    var random11 = getRandom(4);
+    var random22 = getRandom(4);
+
+    this.tables = new Array(4);
+    for (var i = 0; i < 4; i++) {
+        var sprites = new Array(4);
+        for (var j = 0; j < 4; j++) {
+            if (i == random1 && j == random11) {
+                sprites[j] = this.newNumber(i, j, 1);
+            } else if (i == random2 && j == random22) {
+                sprites[j] = this.newNumber(i, j, 1);
+            } else {
+                sprites[j] = this.newNumber(i, j, 0);
+            }
+        }
+        this.tables[i] = sprites;
+    }
+
+    this.totalScore = 0;
+
+};
+
+
+MainLayer.prototype.newNumber = function (i, j, num) {
+    var cell = cc.MySprite.create(this.rootNode, "5.png", this.getPosition(i, j), 1);
+    var cellLabel = cc.MySprite.createLabel(cell, "");
+
+    if (num > 0) {
+        cell.setColor(COLOR[num]);
+        cellLabel.setVisible(true);
+        cellLabel.setString(this.indexVersion.array[num]);
+        cellLabel.setFontSize(this.indexVersion.labelFontSize);
+    } else {
+        cellLabel.setVisible(false);
+    }
+    cell.data = {col: i, row: j, numberLabel: cellLabel, number: num};
+    return cell;
+};
+
+//direction left
+MainLayer.prototype.leftCombineNumber = function () {
+    for (var j = 0; j < 4; j++) {
+        for (var i = 0; i < 4; i++) {
+            var cell = this.tables[i][j];
+            if (cell.data.number != 0) {
+                var k = i + 1;
+                while (k < 4) {
+                    var nextCell = this.tables[k][j];
+                    if (nextCell.data.number != 0) {
+                        if (cell.data.number == nextCell.data.number) {
+                            cell.data.number += 1;
+                            nextCell.data.number = 0;
+                            this.totalScore += SCORES[cell.data.number];
+                        }
+                        k = 4;
+                        break;
+                    }
+                    k++;
+                }
+            }
+        }
+    }
+
+    for (j = 0; j < 4; j++) {
+        for (i = 0; i < 4; i++) {
+            cell = this.tables[i][j];
+            if (cell.data.number == 0) {
+                k = i + 1;
+                while (k < 4) {
+                    nextCell = this.tables[k][j];
+                    if (nextCell.data.number != 0) {
+                        cell.data.number = nextCell.data.number;
+                        nextCell.data.number = 0;
+                        k = 4;
+                    }
+                    k++;
+                }
+            }
+        }
+    }
+
+    this.refreshNumber();
+};
+
+//direction right
+MainLayer.prototype.rightCombineNumber = function () {
+    for (var j = 0; j < 4; j++) {
+        for (var i = 3; i >= 0; i--) {
+            var cell = this.tables[i][j];
+            if (cell.data.number != 0) {
+                var k = i - 1;
+                while (k >= 0) {
+                    var nextCell = this.tables[k][j];
+                    if (nextCell.data.number != 0) {
+                        if (cell.data.number == nextCell.data.number) {
+                            cell.data.number += 1;
+                            nextCell.data.number = 0;
+                            this.totalScore += SCORES[cell.data.number];
+                        }
+                        k = -1;
+                        break;
+                    }
+                    k--;
+                }
+            }
+        }
+    }
+
+    for (j = 0; j < 4; j++) {
+        for (i = 3; i >= 0; i--) {
+            cell = this.tables[i][j];
+            if (cell.data.number == 0) {
+                k = i - 1;
+                while (k >= 0) {
+                    nextCell = this.tables[k][j];
+                    if (nextCell.data.number != 0) {
+                        cell.data.number = nextCell.data.number;
+                        nextCell.data.number = 0;
+                        k = -1;
+                    }
+                    k--;
+                }
+            }
+        }
+    }
+
+    this.refreshNumber();
+};
+
+MainLayer.prototype.downCombineNumber = function () {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            var cell = this.tables[i][j];
+            if (cell.data.number != 0) {
+                var k = j + 1;
+                while (k < 4) {
+                    var nextCell = this.tables[i][k];
+                    if (nextCell.data.number != 0) {
+                        if (cell.data.number == nextCell.data.number) {
+                            cell.data.number += 1;
+                            nextCell.data.number = 0;
+                            this.totalScore += SCORES[cell.data.number];
+                        }
+                        k = 4;
+                        break;
+                    }
+                    k++;
+                }
+            }
+        }
+    }
+
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            cell = this.tables[i][j];
+            if (cell.data.number == 0) {
+                k = j + 1;
+                while (k < 4) {
+                    nextCell = this.tables[i][k];
+                    if (nextCell.data.number != 0) {
+                        cell.data.number = nextCell.data.number;
+                        nextCell.data.number = 0;
+                        k = 4;
+                    }
+                    k++;
+                }
+            }
+        }
+    }
+
+    this.refreshNumber();
+};
+
+//touch up
+MainLayer.prototype.upCombineNumber = function () {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 3; j >= 0; j--) {
+            var cell = this.tables[i][j];
+            if (cell.data.number != 0) {
+                var k = j - 1;
+                while (k >= 0) {
+                    var nextCell = this.tables[i][k];
+                    if (nextCell.data.number != 0) {
+                        if (cell.data.number == nextCell.data.number) {
+                            cell.data.number += 1;
+                            nextCell.data.number = 0;
+                            this.totalScore += SCORES[cell.data.number];
+                        }
+                        k = -1;
+                        break;
+                    }
+                    k--;
+                }
+            }
+        }
+    }
+
+    for (i = 0; i < 4; i++) {
+        for (j = 3; j >= 0; j--) {
+            cell = this.tables[i][j];
+            if (cell.data.number == 0) {
+                k = j - 1;
+                while (k >= 0) {
+                    nextCell = this.tables[i][k];
+                    if (nextCell.data.number != 0) {
+                        cell.data.number = nextCell.data.number;
+                        nextCell.data.number = 0;
+                        k = -1;
+                    }
+                    k--;
+                }
+            }
+        }
+    }
+
+    this.refreshNumber();
+};
+
+MainLayer.prototype.refreshNumber = function () {
+    var emptyCellList = [];
+    for (var i = 0; i < 4; i++) {
+        var numbers = " ";
+        for (var j = 0; j < 4; j++) {
+            var cell = this.tables[i][j];
+            var label = cell.data.numberLabel;
+            var cellNumber = cell.data.number;
+            if (cellNumber != 0) {
+                cell.setColor(COLOR[cellNumber]);
+                label.setString(this.indexVersion.array[cellNumber] + " ");
+                label.setFontSize(this.indexVersion.labelFontSize);
+                label.setVisible(true);
+                if (cellNumber == (this.indexVersion.array.length - 1)) {
+                    //check success
+                    var toast = cc.Toast.create(this.rootNode, "成功到达：" + this.indexVersion.array[cellNumber], 2);
+                    toast.setColor(cc.c3b(255, 0, 0));
+                    this.rootNode.scheduleOnce(function () {
+                        cc.BuilderReader.runScene("", "MainLayer");
+                    }, 2)
+                }
+            } else {
+                cell.setColor(COLOR[cellNumber]);
+                label.setVisible(false);
+                emptyCellList.push(cell);
+            }
+            numbers += "  " + cellNumber;
+        }
+        cc.log("numbers==" + numbers);
+    }
+
+
+    //score
+    this.scoreLabel.setString("分数：" + this.totalScore);
+
+    if (emptyCellList.length < 1) {
+        //check fail
+        var toast = cc.Toast.create(this.rootNode, "失败！", 2);
+        toast.setColor(cc.c3b(255, 0, 0));
+        this.rootNode.scheduleOnce(function () {
+            cc.BuilderReader.runScene("", "MainLayer");
+        }, 2)
+    } else {
+        //create random cell
+        var randomCell = emptyCellList[getRandom(emptyCellList.length)];
+        randomCell.data.number = 1;
+        randomCell.data.numberLabel.setVisible(true);
+        randomCell.data.numberLabel.setString(VERSIONS[this.versionNum].array[1] + "");
+        randomCell.data.numberLabel.setFontSize(this.indexVersion.labelFontSize);
+        randomCell.setColor(COLOR[1]);
+        randomCell.runAction(cc.Sequence.create(cc.ScaleTo.create(0, 0.8), cc.ScaleTo.create(0.5, 1)));
+
+    }
+
+};
+
+MainLayer.prototype.onRefreshClicked = function () {
+    cc.BuilderReader.runScene("", "MainLayer");
+};
+
+MainLayer.prototype.onTouchesBegan = function (touches, event) {
+    this.pBegan = touches[0].getLocation();
+
+    //back
+    var backRect = cc.rectCreate(this.back.getPosition(), [50, 30]);
+    if (cc.rectContainsPoint(backRect, this.pBegan)) {
+        this.back.runAction(cc.Sequence.create(cc.ScaleTo.create(0.2, 1.1),
+            cc.CallFunc.create(function () {
+                cc.AudioEngine.getInstance().stopAllEffects();
+                cc.BuilderReader.runScene("", "LevelLayer");
+            })
+        ));
+    }
+};
+
+MainLayer.prototype.onTouchesMoved = function (touches, event) {
+};
+
+MainLayer.prototype.onTouchesEnded = function (touches, event) {
+    this.pEnded = touches[0].getLocation();
+    if (this.pBegan) {
+        if (this.pEnded.x - this.pBegan.x > 50) {
+            this.rightCombineNumber();
+        }
+
+        else if (this.pEnded.x - this.pBegan.x < -50) {
+            this.leftCombineNumber();
+        }
+
+        else if (this.pEnded.y - this.pBegan.y > 50) {
+            this.upCombineNumber();
+        }
+
+        else if (this.pEnded.y - this.pBegan.y < -50) {
+            this.downCombineNumber();
+        }
+    }
+};
+
+
+
+
+
+
+
+
